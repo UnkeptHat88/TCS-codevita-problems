cd66ea4294b0de0b2c32b7a2d675aeca6d3beb93
Kiran_Joy <51417521+Kiran88joy@users.noreply.github.com>
2020-06-10 13:48:24 +0530

    Create LexiString.cpp

---
+++ LexiString.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void solve(){
+	string p,s;
+	cin>>p>>s;
+	for(unsigned int i=0;i<p.length();i++){
+		for(unsigned int j=0;j<s.length();j++){
+			if(p[i]==s[j]){
+				cout<<p[i];
+			}
+		}
+	}
+	cout<<endl;
+}
+
+int main(){
+
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++){
+		solve();
+	}
+
+	return 0; 
+}
